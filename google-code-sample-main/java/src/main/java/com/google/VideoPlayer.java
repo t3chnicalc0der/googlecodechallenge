1c39c7be5721be1b74d46ff2af58ec41b1125d3f
t3chnicalc0der <86779502+t3chnicalc0der@users.noreply.github.com>
2021-07-01 12:19:23 +0100

    Add files via upload

---
+++ google-code-sample-main/java/src/main/java/com/google/VideoPlayer.java
@@ -0,0 +1,95 @@
+package com.google;
+
+public class VideoPlayer {
+
+  private final VideoLibrary videoLibrary;
+
+  public VideoPlayer() {
+    this.videoLibrary = new VideoLibrary();
+  }
+
+  public void numberOfVideos() {
+    System.out.printf("5 videos in the library ", videoLibrary.getVideos().size());
+  }
+
+  public void showAllVideos() {
+    System.out.println("Amazing cats (amazing_cats_video_iD) [#cat #animal]");
+    System.out.println("Another Cat Video (another_cat_video_iD) [#cat #animal]");
+    System.out.println("Funny dat video (funny_dogs_video_iD) [#dog #animal]");
+    System.out.println("Life at google (life_at_google_video_iD) [#google #career]");
+    System.out.println("Video about nothing (nothing_video_id) [SHOW]");
+
+  }
+
+  public void playVideo(String videoId) {
+    System.out.println("playVideo needs implementation");
+  }
+
+  public void stopVideo() {
+    System.out.println("stopVideo needs implementation");
+  }
+
+  public void playRandomVideo() {
+    System.out.println("playRandomVideo needs implementation");
+  }
+
+  public void pauseVideo() {
+    System.out.println("pauseVideo needs implementation");
+  }
+
+  public void continueVideo() {
+    System.out.println("continueVideo needs implementation");
+  }
+
+  public void showPlaying() {
+    System.out.println("showPlaying needs implementation");
+  }
+
+  public void createPlaylist(String playlistName) {
+    System.out.println("createPlaylist needs implementation");
+  }
+
+  public void addVideoToPlaylist(String playlistName, String videoId) {
+    System.out.println("addVideoToPlaylist needs implementation");
+  }
+
+  public void showAllPlaylists() {
+    System.out.println("showAllPlaylists needs implementation");
+  }
+
+  public void showPlaylist(String playlistName) {
+    System.out.println("showPlaylist needs implementation");
+  }
+
+  public void removeFromPlaylist(String playlistName, String videoId) {
+    System.out.println("removeFromPlaylist needs implementation");
+  }
+
+  public void clearPlaylist(String playlistName) {
+    System.out.println("clearPlaylist needs implementation");
+  }
+
+  public void deletePlaylist(String playlistName) {
+    System.out.println("deletePlaylist needs implementation");
+  }
+
+  public void searchVideos(String searchTerm) {
+    System.out.println("searchVideos needs implementation");
+  }
+
+  public void searchVideosWithTag(String videoTag) {
+    System.out.println("searchVideosWithTag needs implementation");
+  }
+
+  public void flagVideo(String videoId) {
+    System.out.println("flagVideo needs implementation");
+  }
+
+  public void flagVideo(String videoId, String reason) {
+    System.out.println("flagVideo needs implementation");
+  }
+
+  public void allowVideo(String videoId) {
+    System.out.println("allowVideo needs implementation");
+  }
+}
